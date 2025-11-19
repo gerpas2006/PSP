070b1c685e189ccea9f0faa84aff759f055bc407
Germán <german.pastor2006@gmail.com>
2025-11-19 13:28:11 +0100

    subida

--- api-monumentos/src/main/java/com/salesianostriana/edu/api_monumentos/Controller/MonumentoController.java
+++ api-monumentos/src/main/java/com/salesianostriana/edu/api_monumentos/Controller/MonumentoController.java
@@ -2,8 +2,18 @@ package com.salesianostriana.edu.api_monumentos.Controller;
 
 
 import com.salesianostriana.edu.api_monumentos.Dto.MonumentoDto;
+import com.salesianostriana.edu.api_monumentos.Error.MonumentoException;
 import com.salesianostriana.edu.api_monumentos.Model.Monumento;
 import com.salesianostriana.edu.api_monumentos.Service.MonumetoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.graphql.GraphQlProperties;
 import org.springframework.http.HttpStatus;
@@ -13,13 +23,38 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/monument/")
+@RequestMapping("/monumento/")
 @RequiredArgsConstructor
+@Tag(name = "Monumento", description = "El controlador de monumentos, para poder realizar todas las operaciones de gestión")
 public class MonumentoController {
 
     private final MonumetoService monumetoService;
 
+
     @GetMapping
+    @Operation(summary = "Obtiene todos los monumentos",
+            description = "Devuelve una lista de monumentos.")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Se han encontrado los monumentos",
+                    content = @Content(
+                            mediaType = "application/json",
+                            array = @ArraySchema(schema = @Schema(implementation = MonumentoDto.class)),
+                            examples = @ExampleObject(
+                                    value = """
+                    [
+                      {
+                        "descripcion": "La Giralda de Sevilla es muy bonita porque yo he estado allí",
+                        "pais": "España"
+                      }
+                    ]
+                    """
+                            )
+                    )
+            ),
+            @ApiResponse(responseCode = "404", description = "No se ha encontrado el monumento")
+    })
     public List<Monumento> getAll(){
         return monumetoService.findAll();
     }
@@ -30,6 +65,8 @@ public class MonumentoController {
     }
 
     @PostMapping
+    @Operation(summary = "Crea un nuevo monumento")
+    @ApiResponse(responseCode = "201", description = "Monumento creado  correctamente")
     public ResponseEntity<Monumento> create(@RequestBody MonumentoDto monumentoDto){
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(monumetoService.save(monumentoDto));
