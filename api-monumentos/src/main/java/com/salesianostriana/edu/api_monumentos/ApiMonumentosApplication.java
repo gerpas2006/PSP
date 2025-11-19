4b59edff0542fb61087d1df18eecfef88c999e6b
Germán <german.pastor2006@gmail.com>
2025-11-19 13:28:40 +0100

    subida

---
+++ api-monumentos/src/main/java/com/salesianostriana/edu/api_monumentos/ApiMonumentosApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.edu.api_monumentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMonumentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMonumentosApplication.class, args);
+	}
+
+}
---
+++ api-monumentos/src/test/java/com/salesianostriana/edu/api_monumentos/ApiMonumentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.edu.api_monumentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMonumentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
